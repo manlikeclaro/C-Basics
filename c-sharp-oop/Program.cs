5f03f78dfcae19f7770bf4bc2c6eac8a04abdff6
manlikeclaro <ckibii08@gmail.com>
2024-06-13 12:15:46 +0300

    [chore] C# OOP Basics initial commit

---
+++ c-sharp-oop/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "Red", 1969);
+            Car Opel = new Car("Astra", "White", 2005);
+
+            Console.WriteLine(Ford.model);
+            Console.WriteLine(Opel.model);
+
+            Console.WriteLine("Enter the name of the car: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the make of the car: ");
+            string make = Console.ReadLine();
+            Console.WriteLine("Enter the hp of the car: ");
+            int hp = Convert.ToInt32(Console.ReadLine());
+
+            CarTwo ferrari = new CarTwo(name, make, hp);
+            Console.WriteLine($"The car is a {ferrari.Name} {ferrari.make} with {ferrari.hp} hp!");
+            Console.ReadKey();
+        }
+    }
+
+    class CarTwo
+    {
+        private string name;
+        public string make;
+        public int hp;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public CarTwo(string nameParam, string makeParam, int hpParam)
+        {
+            Name = nameParam;
+            make = makeParam;
+            hp = hpParam;
+        }
+    }
+}
---
+++ c-sharp-oop/Program2.cs
@@ -0,0 +1,15 @@
+namespace c_sharp_fundamentals;
+
+class Car
+{
+    public string model;
+    public string color;
+    public int year;
+
+    public Car(string modelName, string modelColor, int modelYear)
+    {
+        model = modelName;
+        color = modelColor;
+        year = modelYear;
+    }
+}
